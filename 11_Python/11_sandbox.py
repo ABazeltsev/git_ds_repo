a6bf489956dd4a90161a375f4102f068f957c088
ABazeltsev <bazeltsev.andrey@gmail.com>
2023-12-18 13:54:26 +0300

    test commit

--- 11_Python/11_sandbox.py
+++ 11_Python/11_sandbox.py
@@ -4,3 +4,6 @@ def get_street_type(adress):
     street_type = adress_list[-1]
     if street_type in exclude_list: street_type = adress_list[-2]
     return street_type
+
+#test for commit
+a = "test string"
