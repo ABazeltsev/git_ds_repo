ac16efb1844f6f5c3d8d1a39aeb681ac731a85dd
b.andrew <trnasp@gmail.com>
2023-12-20 18:03:01 +0300

    20.12.2023

--- 1_python/sbx.py
+++ 1_python/sbx.py
@@ -3,4 +3,22 @@
 # c = a // b
 # e = a % b
 # print(b, " x ", c, " + ", e)
-print(-7//2)
+# print(-7//2)
+# print ((31 % 2) + (-31 % 2))
+# print (13 % -3 * 3 - 3**2)
+#print(3**3-3*(6*3-4.5*2) == 1)
+
+# var1 = 'STROKA'
+# var2 = 'VTORAYA STROKA'
+# print(f'----{var1}-------{var2}')
+
+#----------------------------------------------
+poem = '''Two roads diverged in a yellow wood,
+And sorry I could not travel both
+And be one traveler, long I stood
+And looked down one as far as I could
+To where it bent in the undergrowth.'''
+poem_list = poem.split('\n')
+print(poem_list)
+#----------------------------------------------
+
