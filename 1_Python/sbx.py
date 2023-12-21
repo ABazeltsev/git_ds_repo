a7367768e29ad2dbfb13ca97a20e249e7f02448f
ABazeltsev <bazeltsev.andrey@gmail.com>
2023-12-21 17:04:07 +0300

    21.12

---
+++ 1_Python/sbx.py
@@ -0,0 +1,26 @@
+# new_str = 'Perfect'
+# print(new_str[5])
+# print(new_str[1:])
+# print(new_str[::2])
+#----------------------
+# s1 = "Per"
+# s2 = "fec"
+# s3 = "fe"
+# s4 = "ct"
+# new_string = s1 + s2 + s4[1:]
+# print(new_string)
+# print(new_string.find('fe'))
+# poem = '''Two roads diverged in a yellow wood,
+# And sorry I could not travel both
+# And be one traveler, long I stood
+# And looked down one as far as I could
+# To where it bent in the undergrowth.'''
+# poem_list = poem.split('\n')
+# print(poem_list)
+#----------------------8.6
+# numbers = '1 2 3 4 5 6 7'
+# num_lst = numbers.split()
+# num_abs = '\n'.join(num_lst)
+# print(num_abs)
+#----------------------8.6
+help(str.format)
