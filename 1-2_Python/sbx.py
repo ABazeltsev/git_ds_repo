63c6cef63b20d90cd24ea44a927745bb8d6994d0
ABazeltsev <bazeltsev.andrey@gmail.com>
2024-08-06 16:34:09 +0300

    tech_recovery

--- 1-2_Python/sbx.py
+++ 1-2_Python/sbx.py
@@ -526,3 +526,54 @@
 #     elif n == 1:
 #         print(n, origin)
 #         break
+
+# client_status = {
+#     103303: 'yes', 
+#     103044: 'no',
+#     100423: 'yes',
+#     103032: 'no',
+#     103902: 'no'
+# }
+# for val in client_status:
+#     if client_status[val] == 'no':
+#         continue
+#     else:
+#         print(val)
+
+
+#mixture_dict = {'a': 15, 'b': 10.5, 'c': '15', 'd': 50, 'e': 15, 'f': '15'}
+## count_numbers = 4
+
+#mixture_dict = {'key1': 24, 'key2': '1.4', 'key3': 14, 'key4': 16.24, 'key6': 124.2414, 'key7': 12.2}
+## count_numbers = 5
+# cnt = 0
+# for key_val in mixture_dict:
+#     if type(mixture_dict[key_val]) == int or type(mixture_dict[key_val]) == float:
+#         print(type(mixture_dict[key_val]))
+#         cnt += 1
+#     else:
+#         continue
+# print(cnt)
+
+
+
+text = """
+The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well.
+
+Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next. First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs. She took down a jar from one of the shelves as she passed; it was labelled `ORANGE MARMALADE', but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to put it into one of the cupboards as she fell past it.
+
+`Well!' thought Alice to herself, `after such a fall as this, I shall think nothing of tumbling down stairs! How brave they'll all think me at home! Why, I wouldn't say anything about it, even if I fell off the top of the house!' (Which was very likely true.)
+"""
+
+text = text.lower()
+text = text.replace(" ", "")
+text = text.replace("\n", "")
+sym_dyct = dict()
+#print(text)
+for sym in text:
+    if sym in sym_dyct:
+        sym_dyct[sym] += 1
+
+    else: sym_dyct[sym] = 1
+print(sym_dyct)
+
